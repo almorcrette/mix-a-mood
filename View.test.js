6d738388f9d9c6206bbe9224e4d96b3abdd3a5c1
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-05-30 18:47:52 +0100

    test: set mood to sad when selected and generated

--- View.test.js
+++ View.test.js
@@ -11,15 +11,28 @@ mockedMoodModel.setMood = jest.fn().mockReturnValue("setMood has been called")
 
 
 describe('View', () => {
-  describe('generateMood', () => {
-    it('calls the .setMood method on the mood model with the emotion parameter', () => {
-      document.body.innerHTML = fs.readFileSync('./index.HTML')
-      const view = new View(mockedMoodModel);
-      const emotionSelectorEl = document.querySelector('#happy');
-      emotionSelectorEl.checked = true;
-      const emotionSubmitEl = document.querySelector('#emotion-submit');
-      emotionSubmitEl.click();
-      expect(view.moodModel.setMood).toHaveBeenCalledWith("happy");
+  describe('.generateMood', () => {
+    describe("sets the mood to the selected emotion when 'Generate' is clicked", () => {
+      it("sets mood to 'happy' when 'happy' radio button selected", () => {
+        document.body.innerHTML = fs.readFileSync('./index.HTML')
+        const view = new View(mockedMoodModel);
+        const happySelectorEl = document.querySelector('#happy');
+        happySelectorEl.checked = true;
+        const emotionSubmitEl = document.querySelector('#emotion-submit');
+        emotionSubmitEl.click();
+        expect(view.moodModel.setMood).toHaveBeenCalledWith("happy");
+      })
+
+      it("sets mood to 'sad' when 'sad' radio button selected", () => {
+        document.body.innerHTML = fs.readFileSync('./index.HTML')
+        const view = new View(mockedMoodModel);
+        const sadSelectorEl = document.querySelector('#sad');
+        sadSelectorEl.checked = true;
+        const emotionSubmitEl = document.querySelector('#emotion-submit');
+        emotionSubmitEl.click();
+        expect(view.moodModel.setMood).toHaveBeenCalledWith("sad");
+      })
+
     })
   })
 })
