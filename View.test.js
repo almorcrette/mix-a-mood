4693b20adcbc268a7295e7e22a8a99ec0fca2e8f
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-06-01 08:21:15 +0100

    feat: mood-display element when emotion selected and generate clicked

--- View.test.js
+++ View.test.js
@@ -45,12 +45,13 @@ describe('View', () => {
 
     })
     describe("displays the mood when 'Generate' is clicked", () => {
+      document.body.innerHTML = fs.readFileSync('./index.HTML')
       const view = new View(mockedMoodModel);
       const happySelectorEl = document.querySelector('#happy');
       happySelectorEl.checked = true;
       const emotionSubmitEl = document.querySelector('#emotion-submit');
       emotionSubmitEl.click();
-      expect(document.querySelector('.mood-display').id) === 'happy';
+      expect(document.querySelector('.mood-display').id) === 'happy-img';
     })
   })
 })
