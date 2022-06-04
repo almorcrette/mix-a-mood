417fd57150e556908ed8c33d3afc124d4ba29000
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-06-04 11:50:48 +0100

    test: thesaurus.findSimilarTo

---
+++ ThesaurusApi.test.js
@@ -0,0 +1,27 @@
+const ThesaurusApi = require('./ThesaurusApi')
+
+require('jest-fetch-mock').enableMocks();
+
+describe('ThesaurusApi', () => {
+  describe('.findSimilarTo', () => {
+    it('fetches words similar to word passed as argument', () => {
+      const thesaurusApi = new ThesaurusApi();
+        fetch.mockResponseOnce(JSON.stringify({
+          "word": "exhausted",
+          "simmilarTo": [
+            "tired",
+            "drained"
+          ]
+        }))
+        thesaurusApi.findSimiarTo('exhausted', (res) => {
+          expect(res).toEqual({
+            "word": "exhausted",
+            "simmilarTo": [
+              "tired",
+              "drained"
+            ]
+          })
+        })
+    })
+  })
+})
