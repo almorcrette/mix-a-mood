9895d849a2b9ca37c3dc0a04bbe530594fb62273
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-06-06 10:36:48 +0100

    setup: servier with express.js

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const router = express.Router();
 
-app.set("styles", path.join(__dirname+"/styles"));
-
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname+'/index.html'));
+app.get('/', (req, res) => {
+  res.send('Hello world');
 });
 
 const port = process.env.PORT || 3030;
 
-app.use('/', router);
 app.listen(port, () => console.log(`Server running on port ${port}`));
