b6b7691f03642a007cd461cd5469acc3d2c73e60
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-06-06 11:49:54 +0100

    setup: sendFile index.html but no style or bundle

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+const port = process.env.PORT || 3030;
+
 
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
-
-const port = process.env.PORT || 3030;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
