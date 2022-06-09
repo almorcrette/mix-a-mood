b3e158aa4163ecb9b5096229889e3607ab1a2d4f
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-06-09 18:16:43 +0100

    assets & feat: load 16 expressions load

--- index.js
+++ index.js
@@ -6,8 +6,21 @@ const HomeViewModel = require('./HomeViewModel.js');
 
 const expressionsLibrary = new ExpressionsLibrary(
   new Expression('happy'),
+  new Expression('grateful'),
   new Expression('sad'),
-  new Expression('tired')
+  new Expression('tired'),
+  new Expression('excited'),
+  new Expression('tearful'),
+  new Expression('nervous'),
+  new Expression('curious'),
+  new Expression('angry'),
+  new Expression('amazed'),
+  new Expression('surprised'),
+  new Expression('enthusiastic'),
+  new Expression('bored'),
+  new Expression('joyous'),
+  new Expression('lustful'),
+  new Expression('unimpressed'),
 )
 
 const thesaurusApi = new ThesaurusApi();
