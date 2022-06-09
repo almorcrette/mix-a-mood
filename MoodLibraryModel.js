d68232394eb6e1fc58c4332006431739e1a59e03
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-06-09 10:26:43 +0100

    test: MoodLibraryModel.prototype.matchInLibrary

--- MoodLibraryModel.js
+++ MoodLibraryModel.js
@@ -6,6 +6,20 @@ class MoodLibraryModel {
     return this.coreExpressions[Math.floor(Math.random()*this.coreExpressions.length)]
   }
 
+  matchInLibrary(arr) {
+    let wordMatch = null;
+    arr.some((similarWord) => {
+      this.coreExpressions.some((coreExpression) => {
+        if (coreExpression === similarWord) {
+          wordMatch = coreExpression;
+        };
+        return wordMatch != null;
+      });
+      return wordMatch != null;
+    });
+    return wordMatch;
+  }
+
 }
 
 module.exports = MoodLibraryModel;
--- MoodLibraryModel.test.js
+++ MoodLibraryModel.test.js
@@ -8,6 +8,7 @@ describe('MoodLibraryModel', () => {
 
     })
     describe('.setMoodToUserThesaurusLibraryMatch', () => {
+      // describe('searches the thesaurus for similar words to the parameter emotion that is in the emotion library')
 
     })
     describe('.selectRandomMood', () => {
@@ -25,7 +26,20 @@ describe('MoodLibraryModel', () => {
 
     })
     describe('.matchInLibrary', () => {
-
+      describe('passes an array of words, returns first word matching a core expression in the library', () => {
+        it("returns 'happy' when passed ['happy'] if 'happy' is a core expression", () => {
+          const moodLibrary = new MoodLibraryModel('happy');
+          expect(moodLibrary.matchInLibrary(['happy'])).toEqual('happy');
+        })
+        it("returns 'sad' when passed ['sad'] if 'sad' is a core expression", () => {
+          const moodLibrary = new MoodLibraryModel('sad');
+          expect(moodLibrary.matchInLibrary(['sad'])).toEqual('sad');
+        })
+        it("returns 'sad' when passed ['depressed', 'sad'] if 'sad' is a core expression but not 'depressed'", () => {
+          const moodLibrary = new MoodLibraryModel('sad');
+          expect(moodLibrary.matchInLibrary(['depressed', 'sad'])).toEqual('sad');
+        })
+      })
     })
   })
 })
