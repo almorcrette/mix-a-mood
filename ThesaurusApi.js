b582246a83b69478a6a041546b77311fc9c0d379
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-07-01 09:35:59 +0100

    config: staging and production env

--- ThesaurusApi.js
+++ ThesaurusApi.js
@@ -1,4 +1,4 @@
-const ENV = require('./.env');
+// const ENV = require('./.env');
 
 class ThesaurusApi {
 
@@ -12,7 +12,7 @@ class ThesaurusApi {
       method: 'GET',
       headers: {
         'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com',
-        'X-RapidAPI-Key': ENV.X_RapidAPI_Key
+        'X-RapidAPI-Key': process.env.X_RapidAPI_Key
       }
     };
     fetch(url, options)
